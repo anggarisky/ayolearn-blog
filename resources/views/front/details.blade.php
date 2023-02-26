77aec17c9d95254173208bcdcb5fe9b825ff52aa
Angga <anggariskysetiawan@gmail.com>
2023-02-26 18:59:52 +0700

    update cms

--- resources/views/front/details.blade.php
+++ resources/views/front/details.blade.php
@@ -25,7 +25,7 @@
         </div>
         <div class="grid grid-cols-1 gap-4 mt-5">
             <div class="content">
-                <img class="w-full mb-5" src="{{ Storage::url($tutorial->thumbnail) }}" alt="">
+                <img class="w-full mb-5" src="{{ Storage::url($details->thumbnail) }}" alt="">
                 {!! $details->content !!} 
             </div>
         </div>
