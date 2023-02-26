a1a0f16e4a7e3210e666d244160226893dbe33a5
Angga <anggariskysetiawan@gmail.com>
2023-02-26 18:54:18 +0700

    update cms

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -10,7 +10,33 @@
     <title>Learn UI/UX Design & Website Development | Ayolearn</title>
 </head>
 <body>
-    @include('components/navbar')
+    <nav class="flex align-middle mx-auto w-9/12 my-8">
+        <a href="{{ route('home') }}">
+            <h2 class="text-indigo-900 text-2xl font-bold">Ayolearn</h2>
+        </a>
+        <ul class="ml-20 mt-2 flex gap-10">
+            <li>
+                <a href="{{ route('home') }}">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    UI/UX Design
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    Web Development
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    Mobile Development
+                </a>
+            </li>
+        </ul>
+    </nav>
     <section class="mt-20 w-9/12 mx-auto mb-20">
         <div class="grid grid-cols-1 gap-4">
             <div class="heading">
