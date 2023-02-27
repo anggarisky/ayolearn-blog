74a24dcefec1f4fefbb6c63ab12267aff833a6ee
Angga <anggariskysetiawan@gmail.com>
2023-02-27 10:14:10 +0700

    vite scss

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -7,7 +7,8 @@
     <meta content="index, follow" name="robots" />
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+    {{-- <link rel="stylesheet" href="{{ asset('css/main.css') }}" /> --}}
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 
     <title>Learn UI/UX Design & Website Development | Ayolearn</title>
 </head>
